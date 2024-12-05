6d1b460c0b2a73c36a5dca9e8ee2c505fe5b52be
stCarolas <stcarolas@gmail.com>
2024-12-05 09:09:23 +0300

    add presets for donation-timer

--- src/stores/PresetStore.ts
+++ src/stores/PresetStore.ts
@@ -993,102 +993,352 @@ export class PresetStore {
           showcase: "https://api.oda.digital/presets/donation-timer-5.gif",
           properties: [
             {
-                "name": "preset",
-                "value": "donation-timer"
+              name: "preset",
+              value: "donation-timer",
             },
             {
-                "name": "resetOnLoad",
-                "value": true
+              name: "resetOnLoad",
+              value: true,
             },
             {
-                "name": "text",
-                "value": "Без донатов уже <time>"
+              name: "text",
+              value: "Без донатов уже <time>",
             },
             {
-                "name": "titleFont",
-                "value": {
-                    "size": 30,
-                    "color": {
-                        "angle": 0,
-                        "colors": [
-                            {
-                                "color": "rgb(255, 255, 255)"
-                            }
-                        ],
-                        "gradient": false,
-                        "repeating": false,
-                        "gradientType": 0
+              name: "titleFont",
+              value: {
+                size: 30,
+                color: {
+                  angle: 0,
+                  colors: [
+                    {
+                      color: "rgb(255, 255, 255)",
                     },
-                    "family": "Cormorant SC",
-                    "italic": false,
-                    "weight": true,
-                    "animation": "none",
-                    "underline": false,
-                    "shadowColor": "rgb(0, 0, 0)",
-                    "shadowWidth": 2,
-                    "animationType": "entire",
-                    "shadowOffsetX": 0,
-                    "shadowOffsetY": 0
-                }
+                  ],
+                  gradient: false,
+                  repeating: false,
+                  gradientType: 0,
+                },
+                family: "Cormorant SC",
+                italic: false,
+                weight: true,
+                animation: "none",
+                underline: false,
+                shadowColor: "rgb(0, 0, 0)",
+                shadowWidth: 2,
+                animationType: "entire",
+                shadowOffsetX: 0,
+                shadowOffsetY: 0,
+              },
             },
             {
-                "name": "backgroundColor",
-                "value": {
-                    "angle": 90,
-                    "colors": [
-                        {
-                            "color": "rgba(255, 255, 255, 0)"
-                        },
-                        {
-                            "color": "rgb(220, 38, 12)"
-                        },
-                        {
-                            "color": "rgba(255, 255, 255, 0)"
-                        }
-                    ],
-                    "gradient": true,
-                    "repeating": false,
-                    "gradientType": 0
-                }
+              name: "backgroundColor",
+              value: {
+                angle: 90,
+                colors: [
+                  {
+                    color: "rgba(255, 255, 255, 0)",
+                  },
+                  {
+                    color: "rgb(220, 38, 12)",
+                  },
+                  {
+                    color: "rgba(255, 255, 255, 0)",
+                  },
+                ],
+                gradient: true,
+                repeating: false,
+                gradientType: 0,
+              },
             },
             {
-                "name": "border",
-                "value": {
-                    "top": {
-                        "type": "solid",
-                        "color": "rgb(255, 255, 255)",
-                        "width": 5
-                    },
-                    "left": {
-                        "type": "solid",
-                        "color": "rgb(255, 255, 255)",
-                        "width": 0
-                    },
-                    "right": {
-                        "type": "solid",
-                        "color": "rgb(255, 255, 255)",
-                        "width": 0
-                    },
-                    "bottom": {
-                        "type": "solid",
-                        "color": "rgb(255, 255, 255)",
-                        "width": 5
-                    },
-                    "isSame": false
-                }
+              name: "border",
+              value: {
+                top: {
+                  type: "solid",
+                  color: "rgb(255, 255, 255)",
+                  width: 5,
+                },
+                left: {
+                  type: "solid",
+                  color: "rgb(255, 255, 255)",
+                  width: 0,
+                },
+                right: {
+                  type: "solid",
+                  color: "rgb(255, 255, 255)",
+                  width: 0,
+                },
+                bottom: {
+                  type: "solid",
+                  color: "rgb(255, 255, 255)",
+                  width: 5,
+                },
+                isSame: false,
+              },
             },
             {
-                "name": "rounding",
-                "value": {
-                    "isSame": null,
-                    "topLeft": 1,
-                    "topRight": 0,
-                    "bottomLeft": 0,
-                    "bottomRight": 0
-                }
-            }
-          ]
-        })
+              name: "rounding",
+              value: {
+                isSame: null,
+                topLeft: 1,
+                topRight: 0,
+                bottomLeft: 0,
+                bottomRight: 0,
+              },
+            },
+          ],
+        }),
+        new Preset({
+          name: "donation-timer-6",
+          showcase: "https://api.oda.digital/presets/donation-timer-6.gif",
+          properties: [
+            {
+              name: "preset",
+              value: "donation-timer",
+            },
+            {
+              name: "text",
+              value: "Без донатов уже <time>",
+            },
+            {
+              name: "titleFont",
+              value: {
+                size: 24,
+                color: {
+                  angle: 0,
+                  colors: [
+                    {
+                      color: "rgb(217, 133, 13)",
+                    },
+                  ],
+                  gradient: false,
+                  repeating: false,
+                  gradientType: 0,
+                },
+                family: "Rubik Mono One",
+                italic: false,
+                weight: true,
+                animation: "none",
+                underline: false,
+                shadowColor: "rgb(0, 0, 0)",
+                shadowWidth: 3,
+                animationType: "entire",
+                shadowOffsetX: 0,
+                shadowOffsetY: 0,
+              },
+            },
+            {
+              name: "textAlign",
+              value: "center",
+            },
+            {
+              name: "backgroundColor",
+              value: {
+                angle: 0,
+                colors: [
+                  {
+                    color: "rgba(255, 255, 255, 0.2)",
+                  },
+                ],
+                gradient: false,
+                repeating: false,
+                gradientType: 0,
+              },
+            },
+            {
+              name: "border",
+              value: {
+                top: {
+                  type: "solid",
+                  color: "#000000",
+                  width: 0,
+                },
+                left: {
+                  type: "solid",
+                  color: "#000000",
+                  width: 0,
+                },
+                right: {
+                  type: "solid",
+                  color: "#000000",
+                  width: 0,
+                },
+                bottom: {
+                  type: "solid",
+                  color: "rgb(217, 13, 13)",
+                  width: 5,
+                },
+                isSame: null,
+              },
+            },
+            {
+              name: "rounding",
+              value: {
+                isSame: true,
+                topLeft: 40,
+                topRight: 40,
+                bottomLeft: 40,
+                bottomRight: 40,
+              },
+            },
+            {
+              name: "padding",
+              value: {
+                top: 4,
+                left: 0,
+                right: 0,
+                bottom: 0,
+                isSame: false,
+              },
+            },
+            {
+              name: "boxShadow",
+              value: {
+                shadows: [
+                  {
+                    x: 0,
+                    y: 0,
+                    blur: 10,
+                    color: "#000000",
+                    inset: false,
+                    spread: 10,
+                  },
+                ],
+              },
+            },
+            {
+              name: "blur",
+              value: {
+                blur: 7,
+                enabled: true,
+              },
+            },
+          ],
+        }),
+        new Preset({
+          name: "donation-timer-7",
+          showcase: "https://api.oda.digital/presets/donation-timer-7.gif",
+          properties: [
+            {
+              name: "preset",
+              value: "donation-timer",
+            },
+            {
+              name: "text",
+              value: "Без донатов уже <time>",
+            },
+            {
+              name: "titleFont",
+              value: {
+                size: 34,
+                color: {
+                  angle: 0,
+                  colors: [
+                    {
+                      color: "rgb(217, 209, 13)",
+                    },
+                  ],
+                  gradient: false,
+                  repeating: false,
+                  gradientType: 0,
+                },
+                family: "Pattaya",
+                italic: false,
+                weight: true,
+                outline: {
+                  color: "rgb(207, 148, 19)",
+                  width: 2,
+                  enabled: true,
+                },
+                animation: "none",
+                underline: false,
+                shadowColor: "rgb(255, 255, 255)",
+                shadowWidth: 50,
+                animationType: "entire",
+                shadowOffsetX: 0,
+                shadowOffsetY: 0,
+              },
+            },
+            {
+              name: "textAlign",
+              value: "center",
+            },
+            {
+              name: "backgroundColor",
+              value: {
+                angle: 0,
+                colors: [
+                  {
+                    color: "rgba(255, 255, 255, 0)",
+                  },
+                ],
+                gradient: false,
+                repeating: false,
+                gradientType: 0,
+              },
+            },
+            {
+              name: "border",
+              value: {
+                top: {
+                  type: "solid",
+                  color: "#000000",
+                  width: 0,
+                },
+                left: {
+                  type: "solid",
+                  color: "#000000",
+                  width: 0,
+                },
+                right: {
+                  type: "solid",
+                  color: "#000000",
+                  width: 0,
+                },
+                bottom: {
+                  type: "solid",
+                  color: "rgb(217, 13, 13)",
+                  width: 5,
+                },
+                isSame: null,
+              },
+            },
+            {
+              name: "rounding",
+              value: {
+                isSame: true,
+                topLeft: 40,
+                topRight: 40,
+                bottomLeft: 40,
+                bottomRight: 40,
+              },
+            },
+            {
+              name: "padding",
+              value: {
+                top: 0,
+                left: 0,
+                right: 0,
+                bottom: 0,
+                isSame: false,
+              },
+            },
+            {
+              name: "boxShadow",
+              value: {
+                shadows: [],
+              },
+            },
+            {
+              name: "blur",
+              value: {
+                blur: 7,
+                enabled: false,
+              },
+            },
+          ],
+        }),
       ];
     }
     return [];
