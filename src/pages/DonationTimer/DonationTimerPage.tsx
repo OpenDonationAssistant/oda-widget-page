dc8ac68764458fa2908093ae198c007c59aff83c
stCarolas <stcarolas@gmail.com>
2025-09-11 17:39:55 +0300

    fix timer

--- src/pages/DonationTimer/DonationTimerPage.tsx
+++ src/pages/DonationTimer/DonationTimerPage.tsx
@@ -4,17 +4,24 @@ import { DonationTimer } from "./DonationTimer";
 import WidgetWrapper from "../../WidgetWrapper";
 import { Widget } from "../../types/Widget";
 import { DonationTimerWidgetSettings } from "../../components/ConfigurationPage/widgetsettings/DonationTimerWidgetSettings";
+import { useState } from "react";
+import { DefaultHistoryStore, HistoryStore } from "../History/HistoryStore";
 
 export default function DonationTimerPage({}) {
-  const { settings } = useLoaderData() as WidgetData;
+  const { settings, recipientId, conf, widgetId } =
+    useLoaderData() as WidgetData;
 
   const widgetSettings = Widget.configFromJson(
     settings,
   ) as DonationTimerWidgetSettings;
 
+  const [store] = useState<HistoryStore>(
+    () => new DefaultHistoryStore(recipientId, widgetId, conf),
+  );
+
   return (
     <WidgetWrapper>
-      <DonationTimer settings={widgetSettings} />
+      <DonationTimer settings={widgetSettings} store={store} />
     </WidgetWrapper>
   );
 }
