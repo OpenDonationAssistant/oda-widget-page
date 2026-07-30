781a47aa89f323c3f314fb36f33374cf22ae99b0
stCarolas <stcarolas@gmail.com>
2026-07-31 02:15:52 +0300

    fix shoutout action name

--- src/pages/Automation/AutomationAction/ShoutoutAction.tsx
+++ src/pages/Automation/AutomationAction/ShoutoutAction.tsx
@@ -1,14 +1,12 @@
 import { ReactNode } from "react";
 import { Renderable } from "../../../utils";
 import { AutomationAction } from "../AutomationState";
-import { observer } from "mobx-react-lite";
-import LabeledContainer from "../../../components/LabeledContainer/LabeledContainer";
 import { makeAutoObservable } from "mobx";
 
 interface ShoutoutActionValue {}
 
 export class ShoutoutAction implements AutomationAction, Renderable {
-  id = "shoutout";
+  id = "twitch-shoutout";
   name = "Сделать Twitch Shoutout";
   markup: ReactNode = (<></>);
   value: ShoutoutActionValue = {};
