bc1a8d6b9fe8d5df95eadfe5bf879547d8b42614
stCarolas <stcarolas@gmail.com>
2026-08-07 14:22:55 +0300

    update key for credits store

--- src/pages/Automation/AutomationAction/MakePinnedMessageAction.tsx
+++ src/pages/Automation/AutomationAction/MakePinnedMessageAction.tsx
@@ -1,10 +1,12 @@
-import { ReactNode } from "react";
+import { ReactNode, useContext, useState } from "react";
 import { Renderable } from "../../../utils";
 import { AutomationAction } from "../AutomationState";
 import LabeledContainer from "../../../components/LabeledContainer/LabeledContainer";
 import { observer } from "mobx-react-lite";
 import { makeAutoObservable } from "mobx";
 import Textarea from "../../../components/Textarea/Textarea";
+import { TokenStoreContext } from "../../../stores/TokenStore";
+import { Select } from "antd";
 
 interface MakePinnedMessageActionValue {
   text: string;
@@ -12,12 +14,48 @@ interface MakePinnedMessageActionValue {
 
 const MakePinnedMessageActionComponent = observer(
   ({ action }: { action: MakePinnedMessageAction }) => {
+    const [sender, setSender] = useState<string | null>(null);
+    const [receiver, setReceiver] = useState<string | null>(null);
+    const tokenStore = useContext(TokenStoreContext);
+    if (!tokenStore) {
+      return null;
+    }
+    const tokens = tokenStore.tokens
+      .filter((token) => token.enabled)
+      .filter((token) => token.system === "Twitch");
+
     return (
       <LabeledContainer displayName="Текст сообщения">
         <Textarea
           value={action.value.text}
           onChange={(value) => (action.value.text = value)}
         />
+        <LabeledContainer displayName="Аккаунт, кто пишет">
+          <Select
+            options={tokens.map((token) => {
+              return {
+                label: token.settings["name"],
+                value: token.id,
+              };
+            })}
+            onChange={(value) => {
+              setSender(value);
+            }}
+          />
+        </LabeledContainer>
+        <LabeledContainer displayName="В каком чате">
+          <Select
+            options={tokens.map((token) => {
+              return {
+                label: token.settings["name"],
+                value: token.id,
+              };
+            })}
+            onChange={(value) => {
+              setReceiver(value);
+            }}
+          />
+        </LabeledContainer>
       </LabeledContainer>
     );
   },
